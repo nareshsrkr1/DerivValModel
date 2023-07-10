14850ee8642bd87f5c58e1c5dd2d576aca72bd92
nareshsrkr1 <138985162+nareshsrkr1@users.noreply.github.com>
2023-07-10 13:48:01 +0530

    Update OptionModelPrediction.py

--- OptionModelPrediction.py
+++ OptionModelPrediction.py
@@ -1,19 +1,29 @@
 import tensorflow as tf
-from keras.models import load_model
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import joblib
-import numpy as np
 
 # Load the model
-loaded_model = tf.keras.models.load_model("option_pricing_model_savedmodel")
-print('model loaded')
+# loaded_model = tf.keras.models.load_model("option_pricing_model_saved_updated")
+loaded_model = tf.keras.models.load_model("option_pricing_model_saved_model")
+
+print('Model loaded')
 
 # Load the scaler
-scaler = StandardScaler()
-scaler_filename = "scaler.save"
+# scaler_filename = "scaler.save"
+scaler_filename = "scaler_model.save"
+
 scaler = joblib.load(scaler_filename)
 
+
+
+# def model_custom_predict(Spot_Price, Strike_Price, Maturity, risk_free_interest,Volatility, ):
+#     inputs_to_model = pd.DataFrame({'Spot_Price': [Spot_Price / Strike_Price],
+#                                     'Strike_Price': [Strike_Price],
+#                                     'Maturity': [Maturity],
+#                                     'risk_free_interest': [risk_free_interest],
+#                                     'Volatility': [Volatility]
+#                                    })
 def model_custom_predict(Spot, Strike, Maturity, Volatility):
     inputs_to_model = pd.DataFrame({'Spot Price': [Spot / Strike],
                                     'Strike Price': [Strike],
@@ -25,4 +35,11 @@ def model_custom_predict(Spot, Strike, Maturity, Volatility):
 
     return value * Strike
 
+# print(model_custom_predict(2500, 2450.5, 2, 0.01,0.6))
 print(model_custom_predict(50, 45, 90, 40))
+
+# 2023-07-10 02:00:46 INFO: Monte Carlo Call Option Value: 853.5145263671875
+# print(model_custom_predict(50, 45, 90, 10))
+# 1900,2031.1,2,0.01,0.6,594.1515
+# 50,35.0,1,0.01,0.1,15.3446
+
